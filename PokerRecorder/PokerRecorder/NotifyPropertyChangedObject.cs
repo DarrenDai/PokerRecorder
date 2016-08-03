64c7d6403739a6407bb9c181a6838e64655689d7
Darren <daihongyu1988@163.com>
2016-08-03 14:12:34 +0800

    Update NotifyPropertyChanged.

--- PokerRecorder/PokerRecorder/NotifyPropertyChangedObject.cs
+++ PokerRecorder/PokerRecorder/NotifyPropertyChangedObject.cs
@@ -2,8 +2,12 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace PokerRecorder
 {
@@ -15,6 +19,17 @@ namespace PokerRecorder
         {
             if (PropertyChanged != null)
                 PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void OnPropertyChanged<T>(Expression<Func<T>> property)
+        {
+            var temp = (property as MemberExpression).Member as PropertyInfo;
+            OnPropertyChanged(temp.Name);
+        }
+
+        public void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            OnPropertyChanged(propertyName);
         }
     }
 }
